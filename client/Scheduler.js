892bfbbac631580ab5e2b7120f772138636be7ca
Michail Gumbs <uisnech@gmail.com>
2012-07-12 16:58:38 -0400

    Set up start and end time limits and  set up queries and fetch collections

--- client/Scheduler.js
+++ client/Scheduler.js
@@ -15,6 +15,7 @@ function genCal (){
 			        $('#createTimeSlot').modal('show');
 			        Meteor.call('findScheduleById',calEvent.id, function (error, result){
 			        	$('#createTimeSlot #title').html("Update "+ result.title);
+			        	$('#createTimeSlot input[type="submit"]').val('Update Request')
 						$('#createTimeSlot #delete').html('<input type="button" class="delete btn btn-danger" id="'+result.id +'" value="Delete"/>');
 			        	$('#datepicker').val(timeZoneAdjust(result.start, 'MM/dd/yyyy'));
 			        	$('#starttime').val(timeZoneAdjust(result.start, 'h:mma'));
@@ -30,6 +31,7 @@ function genCal (){
 					right: 'agendaWeek,agendaDay'
 				},
 				defaultView: 'agendaWeek',
+				ignoreTimezone: false,
 				events: eventsList
 			});
 		});
@@ -37,14 +39,19 @@ function genCal (){
 };
 
 function timeZoneAdjust(dateString, format) {
+	dateString *= 1000;
+	console.log(dateString);
 	var dateObject = new Date(dateString);
-	var offSet = dateObject.getTimezoneOffset() ;
-	dateObject.setMinutes(offSet);
+	console.log(dateObject);
+	//var offSet = dateObject.getTimezoneOffset() ;
+	//console.log(offSet);
+	//dateObject.setMinutes(offSet);
 	return formatDate(dateObject, format);
 }
 
 Template.Main.events = {
 	'click #createTimeSlotLink':function (event){
+		$('#createTimeSlot input[type="submit"]').val('Submit Request')
 		$('#createTimeSlot #delete').html('');
 		$('#createTimeSlot #title').html('Request a meeting time');
 		$('#datepicker').val('');
@@ -76,17 +83,166 @@ Template.timeForm.events = {
 		var starttime = $('#starttime').val();
 		var endtime = $('#endtime').val();
 		var occupants = $('#occupants option:selected').val();
-		var start = getDateFromFormat(day + ' ' + starttime, 'M/dd/yyyy h:mma');
-		var end = getDateFromFormat(day + ' ' + endtime, 'M/dd/yyyy h:mma');
-		start = new Date(start);
-		end = new Date(end);
+		var start = getDateFromFormat(day + ' ' + starttime+':00', 'M/dd/yyyy h:mma:ss');
+		var end = getDateFromFormat(day + ' ' + endtime+':00', 'M/dd/yyyy h:mma:ss');
+		end /= 1000;
+		start /= 1000;
 		//alert('Date: ' + day + '\nStart: ' + start + '\nEnd: ' + end + '\nOccupants: ' + occupants);
-		Meteor.call('insertSchedule', title , start, end, occupants);
+		if ($('#createTimeSlot input[type="submit"]').val() == 'Submit Request') {
+			Meteor.call('insertSchedule', title , start, end, occupants);
+		} else if ($('#createTimeSlot input[type="submit"]').val() == 'Update Request') {
+			var updateId = $('#delete input').attr("id");
+			Meteor.call('updateSchedule', updateId, start, end, occupants);
+		}
 		$('#createTimeSlot').modal('hide');
 		genCal();
+	},
+	'blur #datepicker' : function(event) {
+		checkUsersStart();
+		checkUsersEnd();
+	},
+	'blur #starttime' : function(event) {
+		checkUsersStart();
+		checkUsersEnd();
+	},
+	'blur #endtime' : function(event) {
+		checkUsersStart();
+		checkUsersEnd();
 	}
 }
+function checkUsersStart() {
+	var day = $('#datepicker').val();
+	var starttime = $('#starttime').val();
+	var endtime = $('#endtime').val();
+	var occupants = $('#occupants option:selected').val();
+	var userCount = 0;
+	var prevCount = 0;
+	var queryEnd, queryStart = {};
+	if (day && starttime && endtime) {
 
+		// Set up start and end time limits
+		var startTimeStamp = getDateFromFormat(day + ' ' + starttime+':00', 'M/dd/yyyy h:mma:ss');
+		var endTimeStamp = getDateFromFormat(day + ' ' + endtime+':00', 'M/dd/yyyy h:mma:ss');
+		startTimeStamp /= 1000;
+		endTimeStamp /= 1000;
+		startTimeStamp += 3600;
+		endTimeStamp += 3600;
+		queryEnd = { "start": { $lt: endTimeStamp } };
+		queryStart = { "end": { $gt: startTimeStamp } };
+
+		//Set up queries and fetch collections
+		var endResult = timeSlots.find(queryEnd);
+		var startResult = timeSlots.find(queryStart);
+		var startResult2 = timeSlots.find(queryStart);
+
+		//Debug Code
+		console.log('Initial Start Time: ' + startTimeStamp);
+		console.log('Initial End Time: ' + endTimeStamp);
+		console.log('Start Results: ' + startResult.count());
+		console.log('End Results: ' + endResult.count());
+
+
+		//Start Iterations to check for layers of events
+		if (startResult.count() > 0) {
+		    console.log("Executing Start Results now!");
+			startResult.forEach(function(startItem) {
+				var tmpCount = 0;
+				if (endResult) {
+					endResult.forEach(function(endItem) {
+						if (startItem.end >= endItem.start && startItem.end <= endItem.end) {
+							tmpCount += parseInt(endItem.occupants);
+						}
+					});
+				};
+				startResult2.forEach(function(startItem2){
+					if (startItem.end <= startItem2.end && startItem.end >= startItem2.start){
+						if (startItem2.start < startTimeStamp) {
+							tmpCount += parseInt(startItem2.occupants);
+						}
+					}
+				});
+
+				console.log('Temp Count: ' + tmpCount);
+
+				if (prevCount < tmpCount) {
+					userCount = tmpCount;
+				}
+				prevCount = tmpCount;
+			});
+		}
+		
+	}
+	console.log('User Count: ' + userCount);
+	return(userCount);
+}
+function checkUsersEnd() {
+	var day = $('#datepicker').val();
+	var starttime = $('#starttime').val();
+	var endtime = $('#endtime').val();
+	var occupants = $('#occupants option:selected').val();
+	var userCount = 0;
+	var prevCount = 0;
+	var queryEnd, queryStart = {};
+	if (day && starttime && endtime) {
+
+		// Set up start and end time limits
+		var startTimeStamp = getDateFromFormat(day + ' ' + starttime+':00', 'M/dd/yyyy h:mma:ss');
+		var endTimeStamp = getDateFromFormat(day + ' ' + endtime+':00', 'M/dd/yyyy h:mma:ss');
+		startTimeStamp /= 1000;
+		endTimeStamp /= 1000;
+		startTimeStamp += 3600;
+		endTimeStamp += 3600;
+		queryEnd = { "start": { $lt: endTimeStamp } };
+		queryStart = { "end": { $gt: startTimeStamp } };
+
+		//Set up queries and fetch collections
+		var endResult = timeSlots.find(queryEnd);
+		var startResult = timeSlots.find(queryStart);
+		var endResult2 = timeSlots.find(queryEnd);
+
+		//Debug Code
+		console.log('Initial Start Time: ' + startTimeStamp);
+		console.log('Initial End Time: ' + endTimeStamp);
+		console.log('Start Results: ' + startResult.count());
+		console.log('End Results: ' + endResult.count());
+
+
+		//Start Iterations to check for layers of events
+
+		if (endResult.count() > 0) {
+		    console.log("Executing End Results now!");
+			endResult.forEach(function(endItem) {
+				var tmpCount = 0;
+				if (startResult) {	
+					startResult.forEach(function(startItem) {
+						if (endItem.start >= startItem.end && endItem.start <= startItem.start) {
+							tmpCount += parseInt(startItem.occupants);
+						}
+					});
+				}
+				endResult2.forEach(function(endItem2){
+					if (endItem.start >= endItem2.start && endItem.start <= endItem2.end){
+						if (endItem2.end > endTimeStamp) {
+							tmpCount += parseInt(endItem2.occupants);
+						}
+					}
+				});
+
+				console.log('Temp Count: ' + tmpCount);
+
+				if (prevCount < tmpCount) {
+					userCount = tmpCount;
+				}
+				console.log('Previous Count: ' + prevCount);
+				console.log('User Count: ' + userCount);
+				prevCount = tmpCount;
+			});
+			
+		}
+	}
+	console.log('User Count: ' + userCount);
+	return(userCount)
+}
 function selectIterate(min,max){
 	var options = '[\n';
 	for(i=min;i<=max;i++){
--- server/server.js
+++ server/server.js
@@ -4,15 +4,10 @@ Meteor.methods({
 			timeSlots.update({_id:result},{$set: {id:result}});
 		});
 	},
-	updateSchedule: function(id, title, start, end, occupants) {
+	updateSchedule: function(id, start, end, occupants) {
 		var currentInfo = timeSlots.findOne({_id:id});
 		if(currentInfo) {
-			title = (!title) ? currentInfo.title : title;
-			start = (!start) ? currentInfo.start : start;
-			end = (!end) ? currentInfo.end : end; 
-			occupants = (!occupants) ? currentInfo.occupants : occupants;
-
-			timeSlots.update({_id:id},{$set: {id:id, title:title, start:start, end:end, occupants:occupants}});
+			timeSlots.update({_id:id},{$set: {start:start, end:end, occupants:occupants}});
 			return true;
 		} else {
 			return false;
@@ -36,5 +31,111 @@ Meteor.methods({
 	},
 	deleteSchedule: function (id) {
 		timeSlots.remove({_id:id});
+	},
+	checkUsers:function(day, starttime, endtime, occupants) {
+		var userCount = 0;
+		var prevCount = 0;
+		var queryEnd, queryStart = {};
+		if (day && starttime && endtime) {
+
+			// Set up start and end time limits
+			var startTimeStamp = getDateFromFormat(day + ' ' + starttime+':00', 'M/dd/yyyy h:mma:ss');
+			var endTimeStamp = getDateFromFormat(day + ' ' + endtime+':00', 'M/dd/yyyy h:mma:ss');
+			startTimeStamp /= 1000;
+			endTimeStamp /= 1000;
+			queryEnd = { "start": { $lt: endTimeStamp } };
+			queryStart = { "end": { $gt: startTimeStamp } };
+
+			//Set up queries and fetch collections
+			var endResult = timeSlots.find(queryEnd);
+			var startResult = timeSlots.find(queryStart);
+			var endResult2 = timeSlots.find(queryEnd);
+			var startResult2 = timeSlots.find(queryStart);
+
+			//Debug Code
+			console.log('Initial Start Time: ' + startTimeStamp);
+			console.log('Initial End Time: ' + endTimeStamp);
+			console.log('Start Results: ' + startResult.count());
+			console.log('End Results: ' + endResult.count());
+
+
+			//Start Iterations to check for layers of events
+			if (startResult.count > 0) {
+			    console.log("Executing Start Results now!");
+				startResult.forEach(function(startItem) {
+					var tmpCount = 0;
+					if (endResult) {
+						endResult.forEach(function(endItem) {
+							console.log('S1 End 1: ' + startItem.end + ' >= ' + 'S1 Start 2: ' + endItem.start);
+							console.log('S1 End 1: ' + startItem.end + ' <= ' + 'S1 End 2: ' + endItem.end);
+							if (startItem.end >= endItem.start && startItem.end <= endItem.end) {
+								tmpCount += endItem.occupants;
+							}
+						});
+					};
+					startResult2.forEach(function(startItem2){
+						console.log('S2 End 1: ' + startItem.end + ' <= ' + 'S2 End 2: ' + startItem2.end);
+						console.log('S2 End 1: ' + startItem.end + ' >= ' + 'S2 Start 2: ' + startItem2.start);
+						if (startItem.end <= startItem2.end && startItem.end >= startItem2.start){
+							if (startItem2.start < startTimeStamp) {
+								tmpCount += startItem2.occupants;
+							}
+						}
+					});
+
+					console.log('Temp Count: ' + tmpCount);
+
+					if (prevCount < tmpCount) {
+						userCount = tmpCount;
+					}
+					prevCount = tmpCount;
+				});
+			}
+
+			endResult.rewind();	
+			endResult2.rewind();
+			startResult.rewind();
+
+			if (endResult.count() > 0) {
+			    console.log("Executing End Results now!");
+				endResult.forEach(function(endItem) {
+					var tmpCount = 0;
+					if (startResult) {	
+						startResult.forEach(function(startItem) {
+							if (endItem.start >= startItem.end) {  e1 = 'Is: true' } else { e1 = 'Is: false' }
+							console.log('E1 Start 1: ' + startItem.end + ' >= ' + 'E1 End 2: ' + startItem.end + ' ' + e1);
+							if (endItem.start <= startItem.start) { e1 = 'Is: true' } else { e1 = 'Is: false' }
+							console.log('E1 Start 1: ' + startItem.end + ' <= ' + 'E1 Start 2: ' + startItem.start + ' ' + e1);
+							if (endItem.start >= startItem.end && endItem.start <= startItem.start) {
+								tmpCount += parseInt(startItem.occupants);
+							}
+						});
+					}
+					endResult2.forEach(function(endItem2){
+							var e2;
+							if (endItem.start >= endItem2.start) { e2 = 'Is: true' } else { e2 = 'Is: false'}
+							console.log('E2 Start 1: ' + endItem.end + ' >= ' + 'E2 Start 2: ' + endItem2.end + ' ' + e2);
+							if (endItem.start <= endItem2.end) { e2 = 'Is: true' } else { e2 = 'Is: false'}
+							console.log('E2 Start 1: ' + endItem.end + ' <= ' + 'E2 End 2: ' + endItem2.start + ' ' + e2);
+						if (endItem.start >= endItem2.start && endItem.start <= endItem2.end){
+							if (endItem2.end > endTimeStamp) {
+								tmpCount += parseInt(endItem2.occupants);
+							}
+						}
+					});
+
+					console.log('Temp Count: ' + tmpCount);
+
+					if (prevCount < tmpCount) {
+						userCount = tmpCount;
+					}
+					console.log('Previous Count: ' + prevCount);
+					console.log('User Count: ' + userCount);
+					prevCount = tmpCount;
+				});
+				
+			}
+		}
+		console.log('User Count: ' + userCount);
 	}
 })
